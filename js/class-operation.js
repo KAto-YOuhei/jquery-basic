592025baf87783bf76fbcb526076ae375b026498
KAto-YOuhei <you42691@gmail.com>
2023-04-20 10:09:06 +0900

    toggleで属性の追加と削除の切り替え

--- js/class-operation.js
+++ js/class-operation.js
@@ -1,13 +1,7 @@
 $(function () {
-  //id属性がaddの要素がクリックされたら
-  $("#add").on("click", function () {
-    //id属性がtargetの要素にclass属性'sample'を追加する
-    $("#target").addClass("sample");
-  });
-
-  //id属性がremoveの要素がクリックされたら
-  $("#remove").on("click", function () {
-    //id属性がtargetの要素からclass属性'sample'を削除
-    $("#target").removeClass("sample");
+  //id属性がtoggleの要素がクリックされたら
+  $("#toggle").on("click", function () {
+    //id属性がtargetの要素にclass属性'sample'を追加/削除する
+    $("#target").toggleClass("sample");
   });
 });
